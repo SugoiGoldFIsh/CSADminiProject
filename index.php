f5395ae9f54604424bbd8f084d40e337c826237b
SugoiGoldFIsh <Xenathlow22@gmail.com>
2021-08-18 15:42:13 +0800

    Index.php update

--- index.php
+++ index.php
@@ -15,113 +15,13 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="HomePageCSS.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-    
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 675px}
-    
-    /* Set gray background color and 100% height */
-    .sidenav {
-      padding-top: 20px;
-      background-color: #f1f1f1;
-      height: 100%;
-      width: 20%
-    }
-    
-    /* Set black background color, white text and some padding */
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-      display: none;
-    }
-    
-    /* On small screens, set height to 'auto' for sidenav and grid */
-    @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-                .row.content {height:auto;}
-    }
-    
-    #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      #description {
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-      }
-
-      #infowindow-content .title {
-        font-weight: bold;
-      }
-
-      .pac-card {
-        margin: 10px 10px 0 0;
-        border-radius: 2px 0 0 2px;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        outline: none;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-        background-color: #fff;
-        font-family: Roboto;
-      }
-
-      #pac-container {
-        padding-bottom: 12px;
-        margin-right: 12px;
-      }
-
-      .pac-controls {
-        display: inline-block;
-        padding: 5px 11px;
-      }
-
-      .pac-controls label {
-        font-family: Roboto;
-        font-size: 13px;
-        font-weight: 300;
-      }
-      
-      #pac-input { 
-        background-color: #fff;
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-        margin-left: 0px;
-        padding: 0 11px 0 13px;
-        text-overflow: ellipsis;
-        width: 400px; 
-      }
-
-      #pac-input:focus {
-        border-color: #4d90fe;
-      }
-      #middle {height:100%; width: 80%}
-      #well {height: 100%;width: 100%;}
-      
-  </style>
 </head>
-
-
 <body>
 
-    
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -130,33 +30,47 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+        <img class="navbar-brand" src="Logo.jpg">
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-          <li class="active"><a href="index.php">Home</a></li>
+        <li class="active"><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Projects</a></li>
         <li><a href="#">Contact</a></li>
-
       </ul>
       <ul class="nav navbar-nav navbar-right">
-          <?php  if (isset($_SESSION['username'])) : ?>
-            <li> <a href="profile.php"><span class="glyphicon glyphicon-user"> <?php echo $_SESSION['username']; ?> </a></li>
-          <?php else: ?>
-            <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-          <?php endif ?>
-        
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
   </div>
 </nav>
   
-<div class="container-fluid text-center">
+    <div class="container-fluid text-center">
     <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+    <script>
+        function openNav(){
+            document.getElementById("middle").style.width = "80%";
+            document.getElementById("Side-Nav").style.width= "20%";
+        }
+    </script>
   <div class="row content">
     <div id="middle" class="col-sm-8"> 
-      <div id="map"></div>
+        <div id="map"></div>
+        <Button id="Side-Nav-Button" onclick="openNav()">Open Sidebar</button>
+        <Button id="Close-Nav-Button" onclick="closeNav()">Close Sidebar</Button>
+        <script>
+            function openNav(){
+                document.getElementById("middle").style.width= "80%";
+                document.getElementById("Side-Nav").style.width= "20%";
+                document.getElementById("Side-Nav").style.display= "block";
+            }
+            function closeNav(){
+                document.getElementById("middle").style.width= "100%";
+                document.getElementById("Side-Nav").style.width= "0%";
+                document.getElementById("Side-Nav").style.display= "none";
+            }
+        </script>
         <script>
             function initAutocomplete() {
                 var map = new google.maps.Map(document.getElementById('map'), {
@@ -178,11 +92,24 @@
                     var Latitude;
                     var Longitude;
                     google.maps.event.addListener(map, 'click', function(event) {
-                        alert("Latitude: " + event.latLng.lat() + " " + ", longitude: " + event.latLng.lng());
+                        //alert("Latitude: " + event.latLng.lat() + " " + ", longitude: " + event.latLng.lng());
                         //Store latitude and longitude into variable
                         Latitude = event.latLng.lat();
                         Longitude = event.latLng.lng();
-                        });
+                        
+                        var SerangoondLat = Math.abs(1.3499488990593538 - Latitude);
+                        var SerangoondLong = Math.abs(103.87345790863037 - Longitude);
+                        var SerangoonMRTTime = 10;
+                        if (SerangoondLat < 0.00005 && SerangoondLong < 0.00005) {
+                            disp.textContent = "Next MRT arrival : "+ SerangoonMRTTime +" minutes";
+                            document.getElementById("MRTImage").src="DoverMRT.jpg";
+                        } else {
+                            disp.textContent = "Click on an MRT station for arrival timing!";
+                        }
+                        lat.textContent = Latitude;
+                        lon.textContent = Longitude;
+                  });
+                        
 
                         var markers = [];
                         // Listen for the event fired when the user selects a prediction and retrieve
@@ -232,22 +159,60 @@
                           });
                           map.fitBounds(bounds);
                         });
+                        /*var SideNavBtn = document.createElement("Button");
+                        SideNavBtn.id = "Side-Nav-Button";
+                        SideNavBtn.onclick = "openNav()";
+                        SideNavBtn.value = "Open Sidebar";
+                        map.appendChild(SideNavBtn);*/
                       }
                        </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8bbmButzgqA9hMwj5jh-nCkg9usIURFg&amp;libraries=places&amp;callback=initAutocomplete"
          async defer></script>
     </div>
-    <div class="col-sm-4 sidenav">
+    <div id="Side-Nav" class="col-sm-2 sidenav">
         <div class="well" id="well">
-          <img src="DoverMRT.jpg" style =" height: auto; width: 100%" >
-      </div>
+          <img id="MRTImage" src="BlankImage.jpg" style =" height: auto; width: 100%" >
+          <br><br>
+          <ul id="unorderedlist">
+              <li>Latitude: <span id="lat"></span></li>
+              <li>Longitude: <span id="lon"></span> </li>
+          </ul>
+          <br>
+          <span id="disp">Click on an MRT for arrival timing!</span>
+          <br>
+          <div class="Rating-container">
+              <div class="star-widget">
+                  <p id="RateUs" class="RateUs">Rate Our Website!</p>
+                  <input type="radio" name="rate" id="rate-5">
+                  <label for="rate-5" class="fas fa-star"></label>
+                  <input type="radio" name="rate" id="rate-4">
+                  <label for="rate-4" class="fas fa-star"></label>
+                  <input type="radio" name="rate" id="rate-3">
+                  <label for="rate-3" class="fas fa-star"></label>
+                  <input type="radio" name="rate" id="rate-2">
+                  <label for="rate-2" class="fas fa-star"></label>
+                  <input type="radio" name="rate" id="rate-1">
+                  <label for="rate-1" class="fas fa-star"></label>
+                  <form action="#" method="post">
+                    <header></header>
+                  <div class="textarea">
+                    <textarea cols="30" placeholder="Describe your experience.."></textarea>
+                  </div>
+                  <div class="Post-btn">
+                      <button type="submit" onclick="Thankfeedback()">Post</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          <script>
+              function Thankfeedback() {
+                  alert("Thank You For Your Feedback!");
+              }
+          </script>
+        </div>
     </div>
-  </div>
 </div>
-
-<footer class="container-fluid text-center">
-  <p>Footer Text</p>
-</footer>
-
+</div>
 </body>
 </html>
+
